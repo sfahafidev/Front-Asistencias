1948bbd8f26d241d9c306a789f8f9cec2758b4f7
sfahafidev <sfahafidev@gmail.com>
2024-01-23 21:28:08 -0300

    Estructura de proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component'),
+        children:[
+            
+        ]
+    },
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    }
+
+];
---
+++ src/app/shared/heavy-loaders/user-loader.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user-louder',
+    standalone: true,
+    imports: [],
+    template: `<h1>Hola Mundo</h1>`
+})
+
+export class UsersLoaderComponent {
+    constructor() { }
+
+
+}
